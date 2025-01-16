8c1a27cf6a36e93470219e6d8415c7cf7546f3ab
Javier Arias <103268935+JavierAriasCol@users.noreply.github.com>
2025-01-16 12:08:06 -0500

    fix (playerRemote): Remove visibility detector

--- widgets/player-local-remote.dart
+++ widgets/player-local-remote.dart
@@ -1,17 +1,5 @@
-// Automatic FlutterFlow imports
-import '/backend/schema/structs/index.dart';
-import '/backend/supabase/supabase.dart';
-import '/flutter_flow/flutter_flow_theme.dart';
-import '/flutter_flow/flutter_flow_util.dart';
-import 'index.dart';
-import '/custom_code/actions/index.dart';
-import '/flutter_flow/custom_functions.dart';
 import 'package:flutter/material.dart';
-// Begin custom widget code
-// DO NOT REMOVE OR MODIFY THE CODE ABOVE!
-
 import 'package:video_player/video_player.dart';
-import 'package:visibility_detector/visibility_detector.dart';
 import 'dart:io';
 
 class LocalVideoPlayer extends StatefulWidget {
@@ -45,6 +33,7 @@ class _LocalVideoPlayerState extends State<LocalVideoPlayer> {
       ..initialize().then((_) {
         if (mounted) {
           setState(() {});
+          controller?.play();
         }
       });
   }
@@ -60,37 +49,13 @@ class _LocalVideoPlayerState extends State<LocalVideoPlayer> {
     if (controller == null || !controller!.value.isInitialized) {
       return const Center(
           child:
-              CircularProgressIndicator(strokeWidth: 0.5, color: Colors.white));
+              CircularProgressIndicator(strokeWidth: 1, color: Colors.white));
     }
 
-    return VisibilityDetector(
-      key: Key(widget.videoPath), // Usar videoPath como identificador único
-      onVisibilityChanged: (visibilityInfo) {
-        if (visibilityInfo.visibleFraction >= 0.8) {
-          controller?.play();
-        } else {
-          controller?.pause();
-        }
-      },
-      child: ValueListenableBuilder(
-        valueListenable: controller!,
-        builder: (context, VideoPlayerValue value, child) {
-          return Stack(
-            children: [
-              Center(
-                child: AspectRatio(
-                  aspectRatio: value.aspectRatio,
-                  child: VideoPlayer(controller!),
-                ),
-              ),
-              if (value.isBuffering)
-                const Center(
-                  child: CircularProgressIndicator(
-                      strokeWidth: 1, color: Colors.white),
-                ),
-            ],
-          );
-        },
+    return Center(
+      child: AspectRatio(
+        aspectRatio: controller!.value.aspectRatio,
+        child: VideoPlayer(controller!),
       ),
     );
   }
