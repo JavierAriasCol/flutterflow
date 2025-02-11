0cd80b4b49b0c50142c1dedb0fba7e0eb56d0355
Javier Arias <103268935+JavierAriasCol@users.noreply.github.com>
2025-02-11 11:44:36 -0500

    Update time_picker.dart

--- actions/time_picker.dart
+++ actions/time_picker.dart
@@ -1,3 +1,6 @@
+
+// video: https://www.youtube.com/watch?v=yl2LOhJl964
+
 // Automatic FlutterFlow imports
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
